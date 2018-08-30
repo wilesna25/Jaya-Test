06c850b023c4eb7463d8c6a32be98a6bbfa20e4e
wilmer andres escobar naranjo <wilesna25@gmail.com>
2018-08-30 14:50:33 -0500

    Using express framework

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var server = require("./server");
 var router = require("./router");
 var handle = require("./requestHandler");
 
-server.iniciar(router.route,handle.createNewWeatherConsultant);
+// server.iniciar(router.route,handle.createNewWeatherConsultant);
 
+server.startServer(router.route,handle.createNewWeatherConsultant);
--- server.js
+++ server.js
@@ -1,25 +1,58 @@
-var http = require("http");
-var url = require("url");
+var express = require('express');
+const app = express();
 
-function iniciar(route,createNewWeatherConsultant) {
-  function onRequest(request, response) {
 
-    var city = (url.parse(request.url).pathname+"").split("/weather/")[1];
+// EXPRESS VERSION
+function startServer(route,createNewWeatherConsultant){
 
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("Resultados por consola ... :) ");
- 	let apiKey = "dec31a85e9894f879fa184614182908";
-  	if (city !== '/favicon.ico' && city !== undefined && city !== null) {
-   		route(createNewWeatherConsultant,apiKey,city);
-  	}
+  // app.get('/', function (req, res) {
+  //    res.send('Hello World');
+  // })
 
-    response.end();
-  }
-  
-  //can you please convert KM to Miles and display the right Miles per hour please
+  app.all(`/weather/:city`, function (req, res) {
+     res.send('Searching for  : '+req.params.city);
+     var city = req.params.city;
+     let apiKey = "dec31a85e9894f879fa184614182908";
+     route(createNewWeatherConsultant,apiKey,city);
+  });
 
-  http.createServer(onRequest).listen(3000);
-  console.log("Servidor Iniciado.");
+  var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Server listening at http://%s:%s", host, port);
+  });
+
 }
+exports.startServer = startServer;
 
-exports.iniciar = iniciar;
+
+
+
+
+
+
+
+
+
+
+
+// SIMPLE VERSION
+// var http = require("http");
+// var url = require("url");
+
+// function iniciar(route,createNewWeatherConsultant) {
+//   function onRequest(request, response) {
+//     var city = (url.parse(request.url).pathname+"").split("/weather/")[1];
+//     response.writeHead(200, {"Content-Type": "text/html"});
+//     response.write("Resultados por consola ... :) ");
+//   	let apiKey = "dec31a85e9894f879fa184614182908";
+//   	if (city !== '/favicon.ico' && city !== undefined && city !== null) {
+//    		route(createNewWeatherConsultant,apiKey,city);
+//   	}
+//     response.end();
+//   }
+//   http.createServer(onRequest).listen(3000);
+//   console.log("Servidor Iniciado.");
+// }
+
+// exports.iniciar = iniciar;
